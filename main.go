5cdf303eef5cd967d9c8370be28ee4b092d37062
SurajanShrestha <surajanshrestha35@gmail.com>
2023-06-05 15:43:59 +0545

    feat: hello api added

--- main.go
+++ main.go
@@ -9,5 +9,10 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "world",
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
