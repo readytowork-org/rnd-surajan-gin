60edc86c9c5cf695ccacfbf2cf26307f62d5961b
SurajanShrestha <surajanshrestha35@gmail.com>
2023-09-15 15:22:41 +0545

    feat: password hashed using bcrypt

--- go.mod
+++ go.mod
@@ -45,10 +45,10 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.9.0 // indirect
-	golang.org/x/text v0.10.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.1 // indirect
--- models/user.go
+++ models/user.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -17,9 +18,13 @@ type User struct {
 // BeforeCreate will set a UUID rather than numeric ID.
 func (user *User) BeforeCreate(db *gorm.DB) (err error) {
 	id, err := uuid.NewRandom()
-	if err != nil {
+	// Create a "[]byte" type from user's password because that is the type "bcrypt" needs to generate hashed passwords.
+	pwInBytes := []byte(user.Password)
+	encryptedPw, pwErr := bcrypt.GenerateFromPassword(pwInBytes, bcrypt.DefaultCost)
+	if err != nil || pwErr != nil {
 		return err
 	}
 	user.ID = id
+	user.Password = string(encryptedPw)
 	return
 }
