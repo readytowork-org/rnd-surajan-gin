8aed308ba5280d60345aaa7f0614c1bb7d14701a
SurajanShrestha <surajanshrestha35@gmail.com>
2023-09-25 13:09:03 +0545

    feat: task status enum-like values added in task model

--- models/task.go
+++ models/task.go
@@ -7,10 +7,19 @@ import (
 
 type Task struct {
 	gorm.Model
-	ID     uuid.UUID `gorm:"primarykey" json:"ID"`
-	Title  string    `json:"title" binding:"required"`
-	UserId uuid.UUID `binding:"required"`
+	ID     uuid.UUID  `gorm:"primarykey" json:"ID"`
+	Title  string     `json:"title" binding:"required"`
+	UserId uuid.UUID  `binding:"required"`
+	Status TaskStatus `json:"status" gorm:"default:to_do"`
 }
+
+type TaskStatus string
+
+const (
+	TaskToDo       TaskStatus = "to_do"
+	TaskInProgress TaskStatus = "in_progress"
+	TaskDone       TaskStatus = "done"
+)
 
 // BeforeCreate will set a UUID rather than numeric ID.
 func (task *Task) BeforeCreate(db *gorm.DB) (err error) {
